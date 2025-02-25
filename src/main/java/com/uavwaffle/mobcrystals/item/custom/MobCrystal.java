b7a3cafcd5fa4024d9c29a5714bc70f0c0270c79
UAVwaffle <68166007+UAVwaffle@users.noreply.github.com>
2025-02-25 13:17:18 -0500

    Major Changes: - MobCrystal no longer can dupe mobs - MobCrystal will reset after placing mob - MobCrystals now place mobs where you click - MobCrystals no longer tame entities Minor Changes: - Cleaned up lots of code

--- src/main/java/com/uavwaffle/mobcrystals/item/custom/MobCrystal.java
+++ src/main/java/com/uavwaffle/mobcrystals/item/custom/MobCrystal.java
@@ -1,8 +1,8 @@
 package com.uavwaffle.mobcrystals.item.custom;
 
 import com.uavwaffle.mobcrystals.MobCrystals;
+import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
@@ -11,36 +11,23 @@ import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
 public class MobCrystal extends Item {
-//    private List<CompoundTag> mobStorage;
-
-//    CompoundTag storedEntityTag = getDefaultInstance().getOrCreateTagElement(MobCrystals.MOD_ID);
 
     public MobCrystal(Properties properties) {
         super(properties);
-//        this.mobStorage = new ArrayList<>();
-//        this.storedEntityTag = getDefaultInstance().getOrCreateTagElement(MobCrystals.MOD_ID);
     }
 
 
-    private void saveDataToCrystal(Mob mob, ItemStack itemstack, @NotNull Player player, @NotNull InteractionHand usedHand) {
-        /* temp close
-        CompoundTag entityTag = new CompoundTag();
-        mob.save(entityTag);
-        mobStorage.add(entityTag);
-
-         */
-
+    private void saveMobToCrystal(Mob mob, ItemStack itemstack, @NotNull Player player, @NotNull InteractionHand usedHand) {
         ItemStack copy = itemstack.copy();
         copy.setCount(1);
-        CompoundTag storedEntityTag = new CompoundTag();//copy.getOrCreateTagElement(MobCrystals.MOD_ID);
+        CompoundTag storedEntityTag = new CompoundTag();
         mob.save(storedEntityTag);
         CompoundTag tag = copy.getOrCreateTagElement(MobCrystals.MOD_ID);
         tag.put("boundentity", storedEntityTag);
@@ -54,102 +41,66 @@ public class MobCrystal extends Item {
             }
         }
 
-//        ItemStack copy = itemstack.copy();
-//        copy.setCount(1);
-//        CompoundTag entityTag = copy.getOrCreateTag();
-//        mob.save(entityTag);
-//        CompoundTag tag = copy.getOrCreateTag();
-//        tag.put("boundentity", entityTag);
-
-//        System.out.println("entityTag: " + entityTag.getAsString());
-//        storedEntityTag.put("stored_mob", entityTag);
-//        System.out.println("storedEntityTag: " + storedEntityTag);
-
-//        System.out.println("entityTag save: " + entityTag.getAsString());
-//        CompoundTag tag = itemstack.getOrCreateTag();
-//        tag.put("boundentity", entityTag);
-//        System.out.println("tag save: " + tag.getAsString());
-
-//        CompoundTag mobTag = itemstack.getOrCreateTag().getCompound("boundentity");
-//        System.out.println("mobTag save: " + mobTag.getAsString());
-
-//        if (tag.contains("boundentity")) {
-//            tag.put("boundentity", entityTag);
-//            System.out.println("tag save: " + tag.getAsString());
-
-//            int id = tag.getInt("BoundEntity");
-//            Entity boundEntity = level.getEntity(id);
-//            if (boundEntity == null) {
-//                tag.remove("BoundEntity");
-//                tag.remove("BoundType");
-//            }
-//        }
-
     }
 
-    private void loadDataFromCrystal(Player player, CompoundTag pTag) {
-
+    private void loadMobFromCrystal(@NotNull Level level, Player player, CompoundTag mobTag, Vec3 clickedPos) {
+        EntityType.create(mobTag,level).ifPresent((entity) -> {
+            entity.setPos(clickedPos);
+            ((ServerLevel)level).addWithUUID(entity);
+        });
     }
 
     @Override
-    public @NotNull InteractionResultHolder<ItemStack> use(@NotNull Level level, Player player, @NotNull InteractionHand pUsedHand) {
-        ItemStack itemstack = player.getItemInHand(pUsedHand);
-//        if(mobStorage.isEmpty()) {
-//            return InteractionResultHolder.fail(itemstack);
-//        }
-        if(level.isClientSide()) {
-            return InteractionResultHolder.success(itemstack);
+    public @NotNull InteractionResult useOn(UseOnContext context) {
+        ItemStack itemstack = context.getItemInHand();
+        Level level = context.getLevel();
+        Player player = context.getPlayer();
+        if (player == null) {
+            return InteractionResult.FAIL;
         }
-        /* temp close
+        InteractionHand usedHand = player.getUsedItemHand();
 
-        EntityType.create(mobStorage.get(mobStorage.size() - 1), level).ifPresent((entity) -> {
-            if (entity instanceof TamableAnimal) {
-                ((TamableAnimal)entity).setOwnerUUID(player.getUUID());
-            }
+        if(level.isClientSide()) {
+            return InteractionResult.SUCCESS;
+        }
 
-            entity.setPos(player.getX(), player.getY() + (double)0.7F, player.getZ());
-            ((ServerLevel)level).addWithUUID(entity);
-        });
-
-         */
-
-        itemstack.getTags().forEach(tag -> {
-            System.out.println("me taag: " + tag);
-        });
-
-//        CompoundTag mobTag = itemstack.getOrCreateTag().getCompound("boundentity");
         CompoundTag tag = itemstack.getTagElement(MobCrystals.MOD_ID);
         if (tag == null) {
-            System.out.println("NUUUUULLLL");
-            return InteractionResultHolder.fail(itemstack);
+            return InteractionResult.FAIL;
         }
         CompoundTag mobTag = tag.getCompound("boundentity");
 
+        loadMobFromCrystal(level, player, mobTag, context.getClickLocation());
 
-        System.out.println("mobTag: " + mobTag.getAsString());
 
-        EntityType.create(mobTag,level).ifPresent((entity) -> {
-            if (entity instanceof TamableAnimal) {
-                ((TamableAnimal)entity).setOwnerUUID(player.getUUID());
+        ItemStack defaultCrystal = this.getDefaultInstance();
+        if (itemstack.getCount() == 1) {
+            player.setItemInHand(usedHand, defaultCrystal);
+        } else {
+            itemstack.shrink(1);
+            if (!player.getInventory().add(defaultCrystal)) {
+                player.drop(defaultCrystal, false);
             }
-
-            entity.setPos(player.getX(), player.getY() + (double)0.7F, player.getZ());
-            ((ServerLevel)level).addWithUUID(entity);
-        });
+        }
 
 
-        return InteractionResultHolder.success(itemstack);
+        return InteractionResult.SUCCESS;
     }
 
-    public @NotNull InteractionResult interactLivingEntity(@NotNull ItemStack pStack, @NotNull Player pPlayer, @NotNull LivingEntity pInteractionTarget, @NotNull InteractionHand pUsedHand) {
-//        mobStorage.add(pInteractionTarget);
-        if (!(pInteractionTarget instanceof Mob mobToSave)) {
+    public @NotNull InteractionResult interactLivingEntity(@NotNull ItemStack itemstack, @NotNull Player player, @NotNull LivingEntity interactionTarget, @NotNull InteractionHand usedHand) {
+        if (!(interactionTarget instanceof Mob mobToSave)) {
             return InteractionResult.FAIL;
         }
-        if (!pPlayer.level().isClientSide()) {
-            saveDataToCrystal(mobToSave, pStack, pPlayer, pUsedHand);
+        CompoundTag tag = itemstack.getTagElement(MobCrystals.MOD_ID);
+        if (tag != null) {
+            return InteractionResult.FAIL;
         }
-        pInteractionTarget.discard();
+
+        if (!player.level().isClientSide()) {
+            saveMobToCrystal(mobToSave, itemstack, player, usedHand);
+        }
+
+        interactionTarget.discard();
         return InteractionResult.SUCCESS;
     }
 }
